ea9006f55f5ce6c1f1e0fe87df526968ce47105b
Aparnaraha <aparnaraha2912@gmail.com>
2025-04-05 23:23:49 +0530

    Days updated

--- DAY-4-CSS/app1.cpp
+++ DAY-4-CSS/app1.cpp
@@ -3,4 +3,14 @@ Function MultistageGraph(n, edges, source, destination):
     
     For i = 0 to n - 1:
         D[i] = infinity  // Initialize all distances to infinity
- 
+    
+    D[source] = 0  // Distance to the source node is 0
+
+    For stage = n - 2 down to 0:  // Start from the second last stage to the first stage
+        For each node u in stage:  // Iterate through each node in the current stage
+            min_distance = infinity
+            For each (neighbor, weight) in edges[stage][u]:  // For each outgoing edge from node u
+                min_distance = min(min_distance, D[neighbor] + weight)    //Calculate the shortest path
+            D[u] = min_distance  // Update the shortest distance for node u
+
+    Return D[destination]  // Return the shortest distance to the destination node
