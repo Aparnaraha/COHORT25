d13ca413eb622c50d7e3b11235a347c062628d26
Aparnaraha <aparnaraha2912@gmail.com>
2025-05-16 23:18:39 +0530

    Days updated

---
+++ DAY-15-JS-BASIC/script.js
@@ -0,0 +1,18 @@
+console.log("first") // this is a log message that is used to print the message to the console
+
+console.error("second") // this is an error message that is used to print the error message to the console
+
+console.warn("third") // this is a warning message that is used to print the warning message to the console
+
+
+// Variable Declaration
+
+let a = 10;
+const b = 20;
+var c = 30;
+console.log(a, b, c);
+
+// display message to the user
+alert("Hello, World!");
+prompt("Enter your name:");
+confirm("Are you sure?");
