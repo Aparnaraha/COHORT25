19b50589cbeb546df8068f13092e42950e654fd2
Aparnaraha <aparnaraha2912@gmail.com>
2025-05-17 23:45:10 +0530

    Day1 updated

--- DAY-15-JS-BASIC/script.js
+++ DAY-15-JS-BASIC/script.js
@@ -5,3 +5,14 @@ console.error("second") // this is an error message that is used to print the er
 console.warn("third") // this is a warning message that is used to print the warning message to the console
 
 
+// Variable Declaration
+
+let a = 10;
+const b = 20;
+var c = 30;
+console.log(a, b, c);
+
+// display message to the user
+alert("Hello, World!");
+prompt("Enter your name:");
+confirm("Are you sure?");
