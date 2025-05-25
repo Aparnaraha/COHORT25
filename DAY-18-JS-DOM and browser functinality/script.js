ce4e420d2c7fc6c23505d276be7c68ab7e2d393b
Aparnaraha <aparnaraha2912@gmail.com>
2025-05-25 22:43:15 +0530

    DayS updated

--- DAY-18-JS-DOM and browser functinality/script.js
+++ DAY-18-JS-DOM and browser functinality/script.js
@@ -17,3 +17,23 @@ console.log(`device info: ${navigator.userAgent}`)
 // screen
 console.log(`screen.width: ${screen.width}`)
 console.log(`screen.height: ${screen.height}`)
+
+// === LOCAL STORAGE ===
+
+/* 
+- localStorage is a web storage API that allows you to store data on the client side.
+- the storage capacity of local storage is 5MB.
+- 
+*/
+// set data
+localStorage.setItem('name', 'John');
+localStorage.setItem('age', '20');
+
+let user = localStorage.getItem('name');
+let age = localStorage.getItem('age');
+console.log(user);
+console.log(age);
+
+// remove item
+localStorage.removeItem('age');
+localStorage.clear();
