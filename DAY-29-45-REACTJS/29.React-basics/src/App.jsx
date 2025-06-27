cead5aba1975b77a18fba928a1c76147c1cd010f
Aparnaraha <aparnaraha2912@gmail.com>
2025-06-27 14:08:28 +0530

    Some changes deleted

---
+++ DAY-29-45-REACTJS/29.React-basics/src/App.jsx
@@ -0,0 +1,29 @@
+// import React, { useState } from 'react'
+
+// const App = () => {
+
+//   return (
+//     <div>App</div>
+//   )
+// }
+
+// export default App
+
+
+import React, { useState } from 'react'
+
+const [users, setusers] = useState(
+  [
+    {name:'john', age:12}
+    {name:'mohn', age:22}
+    {name:'rohn', age:14}
+  ]
+)
+
+const App = () => {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
