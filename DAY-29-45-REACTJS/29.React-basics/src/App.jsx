0e0ddf2111c62ce0661f83013082ba5470b2db89
Aparnaraha <aparnaraha2912@gmail.com>
2025-07-04 23:58:41 +0530

    DayS updated

--- DAY-29-45-REACTJS/29.React-basics/src/App.jsx
+++ DAY-29-45-REACTJS/29.React-basics/src/App.jsx
@@ -1,45 +1,22 @@
-// import React, { useState } from 'react'
-
-// const App = () => {
-
-//   return (
-//     <div>App</div>
-//   )
-// }
-
-// export default App
-
-
-import React, { useState } from 'react'
-
+import { useState } from "react";
 
 const App = () => {
-  const [users, setUsers] = useState([
-    { name: 'john', age: 12 },
-    { name: 'mohn', age: 22 },
-    { name: 'rohn', age: 14 }
-  ])
+  const [username, setUsername] = useState("api");
 
-  // Function to add a new user
-  const addUser = () => {
-    const newUser = { name: 'new user', age: 18 }
-    setUsers([...users, newUser]) // Add new user to the existing array
-  }
+  const AdduserName = () => {
+    const newUser =  "aparna" ;
+    setUsername([newUser]);
+  };
 
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">User List</h1>
-
-      {users.map((user, index) => (
-        <div key={index}>
-          <p>Name: {user.name}</p>
-          <p>Age: {user.age}</p>
-        </div>
-      ))} 
-
-      <button onClick={addUser}>Add User</button>
+      <h1 className="text-3xl font-medium ">Username</h1>
+      <h2 className="m-2 p-2">{username}</h2>
+      <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white px-4 py-2 border border-blue-500 hover:border-transparent rounded shadow" onClick={AdduserName}>
+        Add User  
+      </button> 
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- DAY-29-45-REACTJS/29.React-basics/src/main.jsx
+++ DAY-29-45-REACTJS/29.React-basics/src/main.jsx
@@ -2,5 +2,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(<App />)
-// If you want to start measuring performance in your app, pass a function
+createRoot(document.getElementById('root')).render(
+  <App/>
+)
