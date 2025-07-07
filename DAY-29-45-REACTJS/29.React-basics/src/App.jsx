5e53b64257ee8f4535131b8ef8847fd447c8cc34
Aparnaraha <aparnaraha2912@gmail.com>
2025-07-07 23:26:28 +0530

    DayS updated

--- DAY-29-45-REACTJS/29.React-basics/src/App.jsx
+++ DAY-29-45-REACTJS/29.React-basics/src/App.jsx
@@ -15,7 +15,7 @@ const App = () => {
       <h2 className="m-2 p-2">{username}</h2>
       <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white px-4 py-2 border border-blue-500 hover:border-transparent rounded shadow" onClick={AdduserName}>
         Add User  
-      </button> 
+      </button>
     </div>
   );
 };
