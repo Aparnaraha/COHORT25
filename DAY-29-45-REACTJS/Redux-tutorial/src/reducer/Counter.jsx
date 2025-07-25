72564bfa27139512318bdbad461d40f0522cb076
Aparnaraha <aparnaraha2912@gmail.com>
2025-07-25 23:17:47 +0530

    Day updated

--- DAY-29-45-REACTJS/Redux-tutorial/src/reducer/Counter.jsx
+++ DAY-29-45-REACTJS/Redux-tutorial/src/reducer/Counter.jsx
@@ -16,3 +16,11 @@ const Counter = () => {
 };
 
 export default Counter;
+// providing a clear and maintainable structure for managing application state.
+// The store can be integrated with React components using the Provider component from react-redux,
+// allowing components to connect to the Redux store and dispatch actions to update the counter state.
+// This setup is particularly useful for applications that require centralized state management,
+// enabling features like undo/redo functionality, time travel debugging, and more complex state interactions.
+// Additionally, the use of Redux Toolkit's configureStore function helps to streamline the process of
+// setting up the store, reducing the amount of boilerplate code needed and promoting best practices in
+// Redux development.
